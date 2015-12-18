a8ccebf83141c784701d9d1a7c9485595edc9ec4
lfx520 <luofangxia@foxmail.com>
2015-12-18 13:08:25 +0800

    homework

---
+++ homework10/js/homework10.js
@@ -0,0 +1,51 @@
+
+
+		
+		
+/*		
+ var circle1 = document.querySelector("#circle1"); 
+  var circle2 = document.querySelector("#circle2"); 
+   var circle3 = document.querySelector("#circle3"); 
+     var picture = document.querySelector("#picture");
+
+circle1.addEventListenter('click',function(){
+
+	picture.src ="image/01.jpg";
+});
+
+circle2.addEventListenter('click',function(){
+
+	picture.src ="image/02.jpg";
+});
+circle3.addEventListenter('click',function(){
+
+	
+	picture.src ="image/03.jpg";
+});*/
+	var circle1 = document.querySelector('#circle1');
+ 		
+		var picture = document.querySelector('#picture');
+		
+	   circle1.addEventListener('click',function(){
+					
+			picture.src = "image/01.jpg";
+				
+ 	});
+	   var circle2 = document.querySelector('#circle2');
+ 		
+		var picture = document.querySelector('#picture');
+
+	   circle2.addEventListener('click',function(){
+				
+			picture.src = "image/02.jpg";
+				
+ 	});
+	   var circle3 = document.querySelector('#circle3');
+ 		
+		var picture = document.querySelector('#picture');
+
+	   circle3.addEventListener('click',function(){
+				
+			picture.src = "image/03.jpg";
+				
+ 	});
