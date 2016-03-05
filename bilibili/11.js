52ff82e2501b0b833c2fbf9bb1d3a84074577215
lfx520 <luofangxia@foxmail.com>
2016-03-05 15:32:54 +0800

    homework

---
+++ bilibili/11.js
@@ -0,0 +1,18 @@
+//window.onload=
+function submit()
+{
+	if (document.form1.myname.value!="罗方霞")//条件1
+    
+	    
+	    	alert("该用户名不存在");//当条件1 为ture时执行
+	    
+	        window.location.href="file:///F:/dend.html";
+
+	    if (document.form2.mypassword.value!="2015210946")//条件2
+
+	        alert("密码错误")//当条件2 为ture时执行 
+	   
+	   else
+	    	window.location.href="http://www.attop.com/dot/gx/index.htm?no=GX0007";//当条件1 和条件2 都不为ture时执行
+	    
+}
---
+++ bilibili/homework.js
@@ -0,0 +1,63 @@
+
+ /*
+  setInterval(test,1000); 
+  var array = new Array(); 
+  var index = 0; 
+  var array = new Array("image/1.jpg","image/2.jpg","image/3.jpg","image/4.jpg","image/5.jpg","image/6.jpg","image/7.jpg","image/8.jpg","image/9.jpg","image/10.jpg"); 
+  function test() { 
+   var myimg=document.getElementById("imgs"); 
+   if(index==array.length-1) 
+   { index=0; }else{ index++; } 
+   myimg.src=array[index]; 
+  } 
+*/    
+
+
+var circle1 = document.querySelector('#circle1');
+ 		
+		var left = document.querySelector('#left');
+		
+	   circle1.addEventListener('click',function(){
+					
+			left.style.backgroundImage="url(img/1.jpg)";
+			circle1.style.background ="#fff";
+				
+ 	});
+
+var circle1 = document.querySelector('#circle2');
+ 		
+		var left = document.querySelector('#left');
+		
+	   circle2.addEventListener('click',function(){
+					
+			left.style.backgroundImage="url(img/13.jpg)";
+			circle2.style.background ="#fff";
+				
+ 	});
+
+var circle1 = document.querySelector('#circle3');
+ 		
+		var left = document.querySelector('#left');
+		
+	   circle3.addEventListener('click',function(){
+					
+			left.style.backgroundImage="url(img/15.jpg)";
+			circle3.style.background ="#fff";
+				
+ 	});
+var doc = document,
+	msgForm = doc.querySelector("#msg-form"),
+	msgWrap = doc.querySelector("#msg-wrap"),
+	msgInput = doc.querySelector("#msg-input"),
+	submitBtn = doc.querySelector("#submit-btn");
+
+
+msgForm.addEventListener("submit", function(event) {
+	event.preventDefault();
+	var message = msgInput.value;
+	var p = document.createElement("p");
+	p.className = "message";
+	p.innerText = message;
+	msgWrap.appendChild(p);
+	
+});
